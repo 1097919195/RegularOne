b92dc48da0eb56c7bb2be2841eb94716cd632633
zjl7577 <1097919195@qq.com>
2018-12-25 14:16:58 +0800

    ServerException类优化

--- library/common/src/main/java/com/jaydenxiao/common/baserx/ServerException.java
+++ library/common/src/main/java/com/jaydenxiao/common/baserx/ServerException.java
@@ -4,6 +4,8 @@ package com.jaydenxiao.common.baserx;
  * des:服务器请求异常
  * Created by xsf
  * on 2016.09.10:16
+ *
+ * 此处最好继承RuntimeException,因为继承Exception需要自己被抛出异常
  */
 public class ServerException extends Exception{
 
