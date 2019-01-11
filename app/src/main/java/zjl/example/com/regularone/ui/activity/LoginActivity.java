8ec60bbe8aa7155b1a2c4fb7d3f0947b8365e78d
zjl7577 <1097919195@qq.com>
2019-01-11 17:27:49 +0800

    login optimize

--- app/src/main/java/zjl/example/com/regularone/ui/activity/LoginActivity.java
+++ app/src/main/java/zjl/example/com/regularone/ui/activity/LoginActivity.java
@@ -1,13 +1,17 @@
 package zjl.example.com.regularone.ui.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.design.widget.TextInputEditText;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.jaydenxiao.common.base.BaseActivity;
 
 import butterknife.BindView;
 import zjl.example.com.regularone.R;
+import zjl.example.com.regularone.utils.EditTextInputHelper;
 
 /**
  * Created by Administrator on 2018/10/30 0030.
@@ -16,6 +20,14 @@ import zjl.example.com.regularone.R;
 public class LoginActivity extends BaseActivity {
     @BindView(R.id.link_signup)
     TextView link_signup;
+    @BindView(R.id.sign_in_button)
+    Button sign_in_button;
+    @BindView(R.id.username_input)
+    TextInputEditText username_input;
+    @BindView(R.id.password_input)
+    TextInputEditText password_input;
+
+    private EditTextInputHelper mEditTextInputHelper;
     @Override
     public int getLayoutId() {
         return R.layout.act_login;
@@ -28,6 +40,11 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void initView() {
+        //设置登录按钮不可点击
+//        sign_in_button.setBackgroundColor(getColor(R.color.gray));
+//        sign_in_button.setClickable(false);
+        mEditTextInputHelper = new EditTextInputHelper(sign_in_button, false);
+        mEditTextInputHelper.addViews(username_input, password_input);
         initListener();
     }
 
